a437c0ef2a6685ce135a6d046886aace4e088a26
Wiktoria Październiak <92630284+wikcia@users.noreply.github.com>
2022-04-28 16:25:00 +0200

    Delete Kurs programowania/Lista_4/Zadanie_3 directory

--- Kurs programowania/Lista_4/Zadanie_3/src/OneParameterFiguresMethods.java
+++
@@ -1,4 +0,0 @@
-public interface OneParameterFiguresMethods {
-    public double area(double edge, Figure.OneParameterFigure oneParameterFigure);
-    public double circuit(double edge, Figure.OneParameterFigure oneParameterFigure);
-}
--- Kurs programowania/Lista_4/Zadanie_3/src/TwoParameterFiguresMethods.java
+++
@@ -1,4 +0,0 @@
-public interface TwoParameterFiguresMethods {
-    public double area(double edge1, double edge2, Figure.TwoParameterFigure twoParameterFigure);
-    public double circuit(double edge1, double edge2, Figure.TwoParameterFigure twoParameterFigure);
-}
